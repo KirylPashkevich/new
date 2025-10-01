c2aa1dd79a5c13634d43eeb38f5043d45590e20c
Kira <119165850+KirylPashkevich@users.noreply.github.com>
2025-10-01 22:32:00 +0300

    Add files via upload

---
+++ MyApp.Tests/Test1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyApp.Tests
+{
+    
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_PositiveNumbers_ReturnsSum()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            int a = 5;
+            int b = 10;
+            int expected = 15;
+
+            // Act
+            int actual = calculator.Add(a, b);
+
+            // Assert
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
